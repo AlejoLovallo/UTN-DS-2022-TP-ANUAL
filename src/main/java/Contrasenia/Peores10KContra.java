170ab65f2ca6ca39ce07469ac953b50ea0866097
casatom <tomas.casa123@gmail.com>
2022-05-08 16:22:15 -0300

    empieza top 10k Peores contraseñas

--- src/main/java/Contrasenia/Peores10KContra.java
+++ src/main/java/Contrasenia/Peores10KContra.java
@@ -1,8 +1,35 @@
 package Contrasenia;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Peores10KContra implements CriterioValidacion {
   //todo
   public boolean validarContrasenia(String contrasenia) {
-    return false;
+
+
+
+    ArrayList<String> peoresContras = new ArrayList<>();
+    String st;
+
+
+    try {
+      File file = new File("10kPasswords.txt");
+      Scanner myReader = new Scanner(file);
+      while (myReader.hasNextLine()) {
+        st = myReader.nextLine();
+        peoresContras.add(st);
+      }
+      myReader.close();
+    } catch (FileNotFoundException e) {
+      //todo poner una mejor excepcion
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+
+    //todo testear 
+    return peoresContras.stream().anyMatch(contrasenia::equals);
   }
 }
