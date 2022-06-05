6dc31432460abb759147dd77d3a5a6348757f221
Sabrina Garcia <mvlts9819@gmail.com>
2022-06-05 20:05:03 -0300

    test de razonsocial,clasificacion y registroSector

--- src/main/java/Domain/Organizacion/Sector.java
+++ src/main/java/Domain/Organizacion/Sector.java
@@ -5,4 +5,10 @@ import Domain.Espacios.Espacio;
 public class Sector {
   private String nombre;
   private Espacio espacioDeTrabajo;
+
+///Constructor
+  public Sector(String _nombre,Espacio _espacioDeTrabajo) {
+    this.nombre=_nombre;
+    this.espacioDeTrabajo=_espacioDeTrabajo;
+  }
 }
--- src/test/java/Organizacion/OrganizacionTest.java
+++ src/test/java/Organizacion/OrganizacionTest.java
@@ -1,11 +1,17 @@
 package Organizacion;
+import Domain.Espacios.Espacio;
+import Domain.Espacios.TipoEspacio;
 import Domain.Organizacion.ClasificacionOrganizacion;
 import Domain.Organizacion.Organizacion;
+import Domain.Organizacion.Sector;
 import Domain.Organizacion.TipoOrganizacion;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Assertions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class OrganizacionTest {
   protected Organizacion organizacionEmpresa;
@@ -35,7 +41,13 @@ public class OrganizacionTest {
 
   @Test
   public void setRazonSocial(){
+    String razonsocialActual=this.organizacionEmpresa.getRazonSocial();
+    String razonsocialNueva="ORG_SA";
 
+    this.organizacionEmpresa.setRazonSocial(razonsocialNueva);
+
+    Assertions.assertEquals(razonsocialNueva,this.organizacionEmpresa.getRazonSocial());
+    Assertions.assertEquals(razonsocialActual,"OrganizacionTest");
   }
 
   @Test
@@ -52,9 +64,25 @@ public class OrganizacionTest {
 
   @Test
   public void setClasificacion(){
+    ClasificacionOrganizacion clasificActual=this.organizacionEmpresa.getClasificacion();
+    ClasificacionOrganizacion clasificNueva=ClasificacionOrganizacion.Ministerio;
+
+    this.organizacionEmpresa.setClasificacion(clasificNueva);
+
+    Assertions.assertEquals(clasificActual, ClasificacionOrganizacion.EmpresaSectorPrimario);
+    Assertions.assertEquals(clasificNueva,this.organizacionEmpresa.getClasificacion());
 
   }
 @Test
-  public void set(){}
+  public void setRegistrarSector(){
+  Espacio espacio=new Espacio("Cordoba",3000, TipoEspacio.Trabajo);
+  Sector sector1=new Sector("Administracion",espacio);
+  Sector sector2=new Sector("direccion",espacio);
+
+  this.organizacionEmpresa.registrarSector(sector1);
+  this.organizacionEmpresa.registrarSector(sector2);
+
+  Assertions.assertEquals(Arrays.asList(sector1,sector2),this.organizacionEmpresa.getSectores());
+}
 
 }
