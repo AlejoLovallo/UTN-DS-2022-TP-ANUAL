d52fbc45de2d7c9aaf89afc6f9af51fe0db2e2a4
Sabrina Garcia <mvlts9819@gmail.com>
2022-06-08 21:47:12 -0300

    tests miembro

--- src/test/java/Miembro/MiembroTest.java
+++ src/test/java/Miembro/MiembroTest.java
@@ -1,14 +1,24 @@
 package Miembro;
 
+import Domain.Espacios.Espacio;
+import Domain.Espacios.TipoEspacio;
+import Domain.Miembro.Excepciones.MiembroNoPerteneceAOrganizacionException;
 import Domain.Miembro.Miembro;
 import Domain.Miembro.TipoDocumento;
+import Domain.Organizacion.ClasificacionOrganizacion;
+import Domain.Organizacion.Organizacion;
+import Domain.Organizacion.Sector;
+import Domain.Organizacion.TipoOrganizacion;
+import Domain.Trayecto.Trayecto;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 public class MiembroTest {
     protected Miembro elMiembro;
@@ -55,7 +65,69 @@ public class MiembroTest {
         Assertions.assertEquals(apellidonuevo,this.elMiembro.getApellido());
     }
 
+    @Test
+    public void setTipoDoc(){
+        TipoDocumento tipodocActual=this.elMiembro.getTipoDocumento();
+        TipoDocumento tipodocNuevo=TipoDocumento.PASAPORTE;
+
+        this.elMiembro.setTipoDocumento(tipodocNuevo);
+
+        Assertions.assertEquals(TipoDocumento.DNI,tipodocActual);
+        Assertions.assertEquals(tipodocNuevo,this.elMiembro.getTipoDocumento());
+    }
+    
+    @Test
+    public void setDoc(){
+        String docActual=this.elMiembro.getDocumento();
+        String docNuevo="123456789";
+
+        this.elMiembro.setDocumento(docNuevo);
+
+        Assertions.assertEquals("456789546",docActual);
+        Assertions.assertEquals(docNuevo,this.elMiembro.getDocumento());
+    }
+
+    @Test
+    public void setTrayecto(){
+        Trayecto trayecto1=new Trayecto();
+        Trayecto trayecto2=new Trayecto();
+        ArrayList<Trayecto> trayectos=new ArrayList<>();
+        trayectos.add(trayecto1);
+        trayectos.add(trayecto2);
+
+        this.elMiembro.setTrayectos(trayectos);
+
+        Assertions.assertEquals(trayectos,this.elMiembro.getTrayectos());
+    }
+
+    @Test
+    public void setSectorxOrg(){
+
+    }
+/*
+    @Test
+    public void registrarTrayecto(){
+        Organizacion org=new Organizacion("OrganizacionTest", TipoOrganizacion.Empresa, ClasificacionOrganizacion.EmpresaSectorPrimario);
+        Trayecto trayecto1=new Trayecto();
+
+        this.elMiembro.registrarTrayecto(org,trayecto1);
+
+        Assertions.assertThrows(throw new MiembroNoPerteneceAOrganizacionException(org.getRazonSocial()),this.elMiembro.registrarTrayecto(org,trayecto1));
+    }
+
+*/
+   /* @Test
+    public void vincularseAorg(){
+        Organizacion org=new Organizacion("OrganizacionTest", TipoOrganizacion.Empresa, ClasificacionOrganizacion.EmpresaSectorPrimario);
+        Espacio espacio=new Espacio("Cordoba",3000, TipoEspacio.Trabajo);
+        Sector sector=new Sector("Administracion",espacio);
+
+        this.elMiembro.vincularseAOrganizacion(org,sector);
+
+        Assertions.assertNull(this.elMiembro.getSectorPorOrganizacion());
 
 
+    }
+*/
 
 }
