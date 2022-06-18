1cb72436f9f4c5aec9eca889e3af1f799a8a8e2a
sebaarossi <sebastiannrossi@gmail.com>
2022-06-17 21:18:45 -0300

    listado provincias/municipios/localidades

--- src/main/java/Domain/CalculadorDistancia/Endpoints/ListadoProvincias.java
+++ src/main/java/Domain/CalculadorDistancia/Endpoints/ListadoProvincias.java
@@ -1,6 +1,7 @@
 package Domain.CalculadorDistancia.Endpoints;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ListadoProvincias {
     private static ListadoProvincias instance = null;
@@ -13,5 +14,24 @@ public class ListadoProvincias {
       return instance;
     }
 
+    public Optional<Provincia> getProvinciaById(String id){
+        return this.provincias.stream()
+                .filter(unaProvincia -> unaProvincia.getId().equals(id))
+                .findFirst();
+    }
+
+    public Optional<Provincia> getProvinciaByName(String name){
+        return this.provincias.stream()
+                .filter(unaProvincia -> unaProvincia.getNombre().equals(name))
+                .findFirst();
+    }
+
+    public void setProvincias(List<Provincia> provincias) {
+        this.provincias = provincias;
+    }
+
+    public List<Provincia> getProvincias(){
+        return provincias;
+    }
 
 }
