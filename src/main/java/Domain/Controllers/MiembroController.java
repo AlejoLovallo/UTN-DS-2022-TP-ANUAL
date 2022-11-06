7143b0115f1dc06fbdf0904179ef5e13923e65db
JoacoTS <joacotruccos@gmail.com>
2022-11-06 15:37:58 -0300

    continua Controller miembro (trayectos y calculoHC)

--- src/main/java/Domain/Controllers/MiembroController.java
+++ src/main/java/Domain/Controllers/MiembroController.java
@@ -1,6 +1,10 @@
 package Domain.Controllers;
 
+import Domain.CalculadorHC.CalculadorHC;
 import Domain.JSON.ParserJSONMiembro;
+import Domain.Miembro.Miembro;
+import Domain.Miembro.Persona;
+import Domain.Repositorios.RepositorioPersonasDB;
 import Domain.Trayecto.Tramo;
 import Domain.Trayecto.Trayecto;
 import org.json.simple.JSONArray;
@@ -10,6 +14,8 @@ import org.json.simple.parser.ParseException;
 import spark.Request;
 import spark.Response;
 
+import java.io.IOException;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
@@ -27,8 +33,8 @@ public class MiembroController {
 
         DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd LLLL yyyy");
         trayecto.setFrecuenciaSemanal((Integer)pedido.get("frecuenciaSemanal"));
-        //trayecto.setFechaInicio(LocalDate.format((String)pedido.get("fechaInicio")), formato);
-        //trayecto.setFechaFin(LocalDate.format(pedido.get("fechaFin", formato)));
+        trayecto.setFechaInicio(LocalDate.parse((String)pedido.get("fechaInicio"), formato));
+        trayecto.setFechaFin(LocalDate.parse((String)pedido.get("fechaFin"), formato));
 
         List<Tramo> tramos = new ArrayList<>();
         ParserJSONMiembro parserJSONMiembro = new ParserJSONMiembro();
@@ -36,5 +42,64 @@ public class MiembroController {
         {
             tramos.add(parserJSONMiembro.JSONATramo((JSONObject)tramo));
         }
+    }
+
+    public void menuRegistrarTrayectos(Request request, Response response)
+    {
+        String username = request.session().attribute("username");
+
+        RepositorioPersonasDB repositorioPersonasDB = new RepositorioPersonasDB();
+        Persona persona = repositorioPersonasDB.buscarPersonaPorUsername(username);
+
+        ParserJSONMiembro parserJSONMiembro = new ParserJSONMiembro();
+        JSONArray listaMiembrosJSON = new JSONArray();
+        for(Miembro miembro : persona.getMiembros())
+        {
+            List<Trayecto> trayectos = miembro.getTrayectos();
+
+            DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd LLLL yyyy");
+            JSONArray trayectosJSON = new JSONArray();
+            for(Trayecto trayecto : trayectos)
+            {
+                JSONObject trayectoObj = new JSONObject();
+                trayectoObj.put("frecuenciaSemanal", trayecto.getFrecuenciaSemanal());
+                trayectoObj.put("fechaInicio", trayecto.getFechaInicio().format(formato));
+                trayectoObj.put("fechaFin", trayecto.getFechaFin().format(formato));
+
+                JSONArray tramos = new JSONArray();
+                for(Tramo tramo : trayecto.getTramos())
+                    tramos.add(parserJSONMiembro.tramoAJSONObject(tramo));
+
+                trayectoObj.put("tramos", tramos);
+
+                trayectosJSON.add(trayectoObj);
+            }
+
+            JSONObject miembroJSON = new JSONObject();
+            miembroJSON.put("organizacion",miembro.getSector().getOrganizacion().getRazonSocial());
+            miembroJSON.put("trayectos", trayectosJSON);
+
+            listaMiembrosJSON.add(miembroJSON);
+        }
+
+        response.type("application/json");
+        response.body(listaMiembrosJSON.toJSONString());
+    }
+
+    public void respuestaCalcularHC(Request request, Response response) throws IOException {
+        String username = request.session().attribute("username");
+        String mes = request.queryParams("Mes");
+        String anio = request.queryParams("Anio");
+
+        RepositorioPersonasDB repositorioPersonasDB = new RepositorioPersonasDB();
+        Persona persona = repositorioPersonasDB.buscarPersonaPorUsername(username);
+
+        Double resultado = 0.0;
+        for(Miembro miembro : persona.getMiembros())
+            resultado += miembro.calcularHC(Integer.parseInt(mes), Integer.parseInt(anio));
+
+        response.type("application/json");
+        response.body("{ \"resultado\":" + resultado.toString() + "}");
+        return;
     }
 }
--- src/main/java/Domain/JSON/ParserJSONMiembro.java
+++ src/main/java/Domain/JSON/ParserJSONMiembro.java
@@ -1,6 +1,8 @@
 package Domain.JSON;
 
 import Domain.Espacios.Direccion;
+import Domain.Espacios.Espacio;
+import Domain.Espacios.Estacion;
 import Domain.MediosDeTransporte.*;
 
 import Domain.Repositorios.RepositorioDireccionDB;
@@ -13,6 +15,7 @@ import org.json.simple.JSONObject;
 
 public class ParserJSONMiembro {
 
+    //----------Desde JSON-------------//
     public MedioDeTransporte JSONAMedioTransporte(JSONObject obj)
     {
         if(obj.get("tipo").equals("vehiculoParticular"))
@@ -63,7 +66,6 @@ public class ParserJSONMiembro {
                 (Integer)jsonDirSalida.get("altura"),
                 (String)jsonDirSalida.get("tipoDireccion")
         );
-        //buscar Espacio por id
 
         Direccion direccionLlegada = repositorioDireccionDB.buscarDireccion(
                 (String)jsonDirLlegada.get("pais"),
@@ -83,5 +85,66 @@ public class ParserJSONMiembro {
 
         return tramo;
     }
+
+    //---------------hacia JSON-------------------//
+    public JSONObject medioTransporteAJSON(MedioDeTransporte medio)
+    {
+        JSONObject obj = new JSONObject();
+        if(medio instanceof VehiculoParticular)
+        {
+            VehiculoParticular aux = (VehiculoParticular) medio;
+            obj.put("tipo", "vehiculoParticular");
+            obj.put("vehiculo", aux.getTipoVehiculo());
+            obj.put("combustible", aux.getTipoCombustible());
+            obj.put("cantidadCompartido", aux.getCantPasajeros());
+        }
+        else if(medio instanceof TransportePublico)
+        {
+            TransportePublico aux = (TransportePublico) medio;
+            obj.put("tipo", "transportePublico");
+            obj.put("vehiculo", aux.getTipoTransportePublico());
+        }
+        else
+        {
+            obj.put("tipo", "biciPie");
+        }
+
+        return obj;
+    }
+
+    public JSONObject EspacioAJSONObject(Espacio espacio)
+    {
+        JSONObject obj = new JSONObject();
+
+        if(espacio instanceof Direccion)
+        {
+            Direccion dir = (Direccion) espacio;
+            obj.put("pais",dir.getPais());
+            obj.put("provincia",dir.getProvincia());
+            obj.put("localidad",dir.getLocalidad());
+            obj.put("calle",dir.getCalle());
+            obj.put("altura",dir.getAltura());
+            obj.put("municipio",dir.getMunicipio());
+            obj.put("tipoDireccion",dir.getTipoDireccion());
+        }
+        else
+        {
+            Estacion estacion = (Estacion) espacio;
+
+            obj.put("nombre", estacion.getNombre());
+        }
+        return obj;
+    }
+
+    public JSONObject tramoAJSONObject(Tramo tramo)
+    {
+        JSONObject tramoObj = new JSONObject();
+        tramoObj.put("puntoSalida", EspacioAJSONObject(tramo.getPuntoPartida()));
+        tramoObj.put("puntoLlegada", EspacioAJSONObject(tramo.getPuntoLlegada()));
+        tramoObj.put("medioTransporte", this.medioTransporteAJSON(tramo.getMedioTransporte()));
+
+        return tramoObj;
+    }
+
 
 }
